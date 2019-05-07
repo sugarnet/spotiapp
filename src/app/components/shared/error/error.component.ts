a8bd9982dac80a66bc202ad08224e3c8b12fd386
Diego Scifo <diegoscifo@gmail.com>
2019-05-07 06:33:36 -0300

    desde casa

---
+++ src/app/components/shared/error/error.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styles: []
+})
+export class ErrorComponent {
+
+  @Input() message: string;
+
+  constructor() { }
+
+
+}
